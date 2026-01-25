fb1603ebc5118c6dacccee0ce69e4f6e4e5b8303
mishmishel <misheltyurkova@gmail.com>
2026-01-25 14:25:32 +1100

    Implemented logic for storing numbers and operators input by user Added functionality to button event listeners, if button is equal to '=', split the display text content into an array and assign first item to num1, second item to operator, and third item to num2.

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,9 @@
 let num1;
 let operator;
 let num2;
+let operation;
+
+let operators = ['/', '*', '+', '-', 'x', '=']
 
 const text = document.querySelector("#text");
 const buttons = document.querySelectorAll("button");
@@ -8,7 +11,21 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         const buttonText = button.textContent;
-        text.textContent += buttonText;
+
+        if (buttonText == '=') {
+            operation = text.textContent.split(" ");
+            num1 = operation[0]
+            operator = operation[1]
+            num2 = operation[2]
+
+            console.log('num1 ' + num1);
+            console.log('operator ' + operator);
+            console.log('num2 ' + num2);
+        } else if (operators.includes(buttonText)) {
+            text.textContent += " " + buttonText + " ";
+        } else {
+            text.textContent += buttonText;
+        }
     })
 })
 
